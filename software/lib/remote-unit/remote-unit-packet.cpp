d7470379595350e075e2db24f1fe7f88e26ae75a
Daniel McMillan <mcmillan.daniel3@gmail.com>
2022-04-26 09:04:00 +1000

    Initial functions for building remote unit data packets

---
+++ software/lib/remote-unit/remote-unit-packet.cpp
@@ -0,0 +1,198 @@
+#include "remote-unit-packet.h"
+#include "crc16.h"
+
+#define PACKET_HEADER_SIZE 2
+#define PACKET_FOOTER_SIZE 3
+#define END_COMMAND_BYTE 0x03
+#define END_RESPONSE_BYTE 0x80 | 0x03
+
+uint16_t read16LE(const uint8_t *data)
+{
+    return (uint16_t)data[0] | (uint16_t)data[1] << 8;
+}
+
+void write16LE(uint8_t *data, uint16_t value)
+{
+    data[0] = (uint8_t)value;
+    data[1] = (uint8_t)(value >> 8);
+}
+
+namespace IrrigationSystem
+{
+    namespace RemoteUnitPacket
+    {
+        enum class RemoteUnitCommand : uint8_t
+        {
+            Invalid = 0x00,
+            // Read operations
+            GetSolenoidState = 0x10,
+            GetBatteryVoltage = 0x11,
+            GetFaults = 0x12,
+            GetBatteryRaw = 0x13,
+            GetSignalStrength = 0x14,
+            GetConfig = 0x15,
+            // Write operations
+            SetSolenoidState = 0x20,
+            ClearFaults = 0x22,
+            SetConfig = 0x25,
+            // Corresponding responses
+            GetSolenoidStateResponse = 0x80 | 0x10,
+            GetBatteryVoltageResponse = 0x80 | 0x11,
+            GetFaultsResponse = 0x80 | 0x12,
+            GetBatteryRawResponse = 0x80 | 0x13,
+            GetSignalStrengthResponse = 0x80 | 0x14,
+            GetConfigResponse = 0x80 | 0x15,
+            SetSolenoidStateResponse = 0x80 | 0x20,
+            ClearFaultsResponse = 0x80 | 0x22,
+            SetConfigResponse = 0x80 | 0x25
+
+        };
+
+        int validatePacket(const uint8_t *packet, size_t packetSize, bool isResponse)
+        {
+            if (CRC::crc16(packet, packetSize))
+            {
+                return -2;
+            }
+            size_t packetOffset = 2;
+            int commandCount = 0;
+            // Check each command
+            while (packetOffset < packetSize - PACKET_FOOTER_SIZE)
+            {
+                uint8_t commandByte = packet[packetOffset];
+                uint8_t dataSize = getCommandDataSize((RemoteUnitCommand)commandByte);
+                // Check command is valid and matches isResponse
+                if (dataSize == 0xff || commandIsResponse(commandByte) != isResponse)
+                {
+                    return -1;
+                }
+                packetOffset += 1 + dataSize;
+                ++commandCount;
+            }
+            // Check footer size
+            if (packetOffset + PACKET_FOOTER_SIZE != packetSize)
+            {
+                return -1;
+            }
+            // Check end command
+            if (packet[packetOffset] != (isResponse ? END_RESPONSE_BYTE : END_COMMAND_BYTE))
+            {
+                return -1;
+            }
+            return commandCount;
+        }
+
+        uint8_t getCommandDataSize(RemoteUnitCommand command)
+        {
+            switch (command)
+            {
+            case RemoteUnitCommand::GetSolenoidState:
+                return 0;
+            case RemoteUnitCommand::GetSolenoidStateResponse:
+                return 1;
+            case RemoteUnitCommand::SetSolenoidState:
+                return 1;
+            case RemoteUnitCommand::SetSolenoidStateResponse:
+                return 1;
+            case RemoteUnitCommand::GetBatteryVoltage:
+                return 0;
+            case RemoteUnitCommand::GetBatteryVoltageResponse:
+                return 1;
+            case RemoteUnitCommand::GetFaults:
+                return 0;
+            case RemoteUnitCommand::GetFaultsResponse:
+                return 1;
+            case RemoteUnitCommand::ClearFaults:
+                return 0;
+            case RemoteUnitCommand::ClearFaultsResponse:
+                return 1;
+            case RemoteUnitCommand::GetBatteryRaw:
+                return 0;
+            case RemoteUnitCommand::GetBatteryRawResponse:
+                return 2;
+            case RemoteUnitCommand::GetSignalStrength:
+                return 0;
+            case RemoteUnitCommand::GetSignalStrengthResponse:
+                return 1;
+            case RemoteUnitCommand::GetConfig:
+                return 0;
+            case RemoteUnitCommand::GetConfigResponse:
+                return 13;
+            case RemoteUnitCommand::SetConfig:
+                return 13;
+            case RemoteUnitCommand::SetConfigResponse:
+                return 13;
+            default:
+                return 0xff;
+            }
+        }
+
+        bool commandIsResponse(uint8_t commandByte)
+        {
+            return commandByte & 0x80 > 0;
+        }
+
+        uint16_t getNodeId(const uint8_t *packet)
+        {
+            return read16LE(packet);
+        }
+
+        RemoteUnitCommand getCommandAtIndex(const uint8_t *packet, size_t commandIndex, const uint8_t **dataOut)
+        {
+            size_t packetOffset = 2;
+            // Find the command
+            while (true)
+            {
+                RemoteUnitCommand command = (RemoteUnitCommand)packet[packetOffset];
+                if (commandIndex == 0)
+                {
+                    *dataOut = packet + packetOffset + 1;
+                    return command;
+                }
+                packetOffset += 1 + getCommandDataSize(command);
+                --commandIndex;
+            }
+        }
+
+        size_t createPacket(uint8_t *packetBuffer, size_t bufferSize, uint16_t nodeId)
+        {
+            if (bufferSize < PACKET_HEADER_SIZE)
+            {
+                return 0;
+            }
+            // Node id
+            write16LE(packetBuffer, nodeId);
+            return PACKET_HEADER_SIZE;
+        }
+
+        size_t addCommandToPacket(uint8_t *packetBuffer, size_t bufferSize, size_t intermediatePacketSize, RemoteUnitCommand command, const uint8_t *data)
+        {
+            uint8_t dataSize = getCommandDataSize(command);
+            size_t newSize = intermediatePacketSize + 1 + dataSize;
+            if (dataSize == 0xff || newSize > bufferSize)
+            {
+                return 0;
+            }
+            // Command
+            packetBuffer[intermediatePacketSize] = (uint8_t)command;
+            // Command data
+            memcpy(packetBuffer + intermediatePacketSize + 1, data, dataSize);
+            return newSize;
+        }
+
+        size_t finalisePacket(uint8_t *packetBuffer, size_t bufferSize, size_t intermediatePacketSize, bool isResponse)
+        {
+            size_t newSize = intermediatePacketSize + PACKET_FOOTER_SIZE;
+            if (newSize > bufferSize)
+            {
+                return 0;
+            }
+            // End command
+            packetBuffer[intermediatePacketSize] = isResponse ? END_RESPONSE_BYTE : END_COMMAND_BYTE;
+            // CRC
+            uint16_t crc = CRC::crc16(packetBuffer, intermediatePacketSize + 1);
+            write16LE(packetBuffer + intermediatePacketSize + 1, crc);
+            return newSize;
+        }
+    }
+}
---
+++ software/lib/remote-unit/remote-unit-packet.h
@@ -0,0 +1,66 @@
+#include <stddef.h>
+#include <inttypes.h>
+#define REMOTE_UNIT_MAX_PACKET_SIZE 64
+
+namespace IrrigationSystem
+{
+    namespace RemoteUnitPacket
+    {
+        /** Identifier of each command. */
+        enum class RemoteUnitCommand : uint8_t;
+
+        /**
+         * Validate a remote unit command or response data packet.
+         *
+         * On success, returns a value >= 0 indicating the number of commands included in the packet.
+         * Returns -1 if the packet includes invalid command or data.
+         * Return -2 if the packet fails CRC.
+         */
+        int validatePacket(const uint8_t *packet, size_t packetSize, bool isResponse);
+
+        /**
+         * Get the size of the data portion of the given command.
+         * Returns 0xff if the given command is invalid.
+         */
+        uint8_t getCommandDataSize(RemoteUnitCommand command);
+
+        /** Check whether the given packet is a response */
+        bool commandIsResponse(uint8_t commandByte);
+
+        /** Get the node id of the given packet */
+        uint16_t getNodeId(const uint8_t *packet);
+
+        /**
+         * Get the command at a certain index within the given packet.
+         * Behaviour is undefined when the index is invalid.
+         * `dataOut` will be set to point to the data portion of the command within the packet.
+         */
+        RemoteUnitCommand getCommandAtIndex(const uint8_t *packet, size_t commandIndex, const uint8_t **dataOut);
+
+        /**
+         * Initialises a new packet for communication with a given node id.
+         *
+         * Returns the intermediate packet size.
+         * Returns 0 if the buffer is too small.
+         */
+        size_t createPacket(uint8_t *packetBuffer, size_t bufferSize, uint16_t nodeId);
+
+        /**
+         * Adds a command to the given packet.
+         * The packet in the buffer should have been initialised with `createPacket` and not yet finalised.
+         *
+         * Returns the intermediate packet size.
+         * Returns 0 if the buffer is too small.
+         */
+        size_t addCommandToPacket(uint8_t *packetBuffer, size_t bufferSize, size_t intermediatePacketSize, RemoteUnitCommand command, const uint8_t *data);
+
+        // Add end part of packet. Return packet size. Return 0 if invalid.
+        /**
+         * Finalises a packet with the end byte and CRC.
+         *
+         * Returns the final packet size.
+         * Returns 0 if the buffer is too small.
+         */
+        size_t finalisePacket(uint8_t *packetBuffer, size_t bufferSize, size_t intermediatePacketSize, bool isResponse);
+    }
+}
