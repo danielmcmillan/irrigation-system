c14431e75b27e87d8781d1a45c848b040e8f1466
Daniel McMillan <mcmillan.daniel3@gmail.com>
2022-04-30 09:45:31 +1000

    Support forcing remote unit solenoid activation

--- software/src/remote-unit/solenoids.cpp
+++ software/src/remote-unit/solenoids.cpp
@@ -2,7 +2,7 @@
 #include "solenoids.h"
 
 Solenoids::Solenoids(const RemoteUnitConfig &config, const SolenoidDefinition (&definitions)[SOLENOID_COUNT])
-    : config(config), definitions(definitions), state(0)
+    : config(config), definitions(definitions), state(SOLENOID_FORCE_FLAG)
 {
 }
 
@@ -30,23 +30,29 @@ void Solenoids::sleep()
 
 void Solenoids::solenoidOn(uint8_t index)
 {
-  const SolenoidDefinition &definition = this->definitions[index];
-  uint8_t pulseWidth = index == 0 ? this->config.getSolenoidAOnPulseWidth() : this->config.getSolenoidBOnPulseWidth();
-  digitalWrite(definition.positivePin, HIGH);
-  digitalWrite(definition.negativePin, LOW);
-  delay(pulseWidth * 2);
-  digitalWrite(definition.positivePin, LOW);
+  if (index < SOLENOID_COUNT)
+  {
+    const SolenoidDefinition &definition = this->definitions[index];
+    uint8_t pulseWidth = index == 0 ? this->config.getSolenoidAOnPulseWidth() : this->config.getSolenoidBOnPulseWidth();
+    digitalWrite(definition.positivePin, HIGH);
+    digitalWrite(definition.negativePin, LOW);
+    delay(pulseWidth * 2);
+    digitalWrite(definition.positivePin, LOW);
+  }
   this->state |= 1 << index;
 }
 
 void Solenoids::solenoidOff(uint8_t index)
 {
-  const SolenoidDefinition &definition = this->definitions[index];
-  uint8_t pulseWidth = index == 0 ? this->config.getSolenoidAOffPulseWidth() : this->config.getSolenoidBOffPulseWidth();
-  digitalWrite(definition.positivePin, LOW);
-  digitalWrite(definition.negativePin, HIGH);
-  delay(pulseWidth * 2);
-  digitalWrite(definition.negativePin, LOW);
+  if (index < SOLENOID_COUNT)
+  {
+    const SolenoidDefinition &definition = this->definitions[index];
+    uint8_t pulseWidth = index == 0 ? this->config.getSolenoidAOffPulseWidth() : this->config.getSolenoidBOffPulseWidth();
+    digitalWrite(definition.positivePin, LOW);
+    digitalWrite(definition.negativePin, HIGH);
+    delay(pulseWidth * 2);
+    digitalWrite(definition.negativePin, LOW);
+  }
   this->state &= ~(1 << index);
 }
 
@@ -57,10 +63,19 @@ uint8_t Solenoids::getState() const
 
 uint8_t Solenoids::setState(uint8_t state)
 {
+  // SOLENOID_FORCE_FLAG bit of state indicates solenoid states should be ignored
+  // This is set as initial state to indicate solenoids states are unknown on startup
+  // It can be set in input state to request forcing solenoid activation on demand
+  if (((this->state | state) & SOLENOID_FORCE_FLAG) != 0)
+  {
+    // Assume current state is opposite of desired to activate all solenoids regardless of current state
+    // Also clear most significant bit since state of all solenoids will now be known
+    this->state = (~state) & ~SOLENOID_FORCE_FLAG;
+  }
   uint8_t toTurnOn = state & ~this->state;
   uint8_t toTurnOff = this->state & ~state;
 
-  for (uint8_t i = 0; i < 8; ++i)
+  for (uint8_t i = 0; i < 7; ++i)
   {
     if ((toTurnOn & (1 << i)) != 0)
     {
--- software/src/remote-unit/solenoids.h
+++ software/src/remote-unit/solenoids.h
@@ -3,6 +3,7 @@
 #include "config.h"
 
 #define SOLENOID_COUNT 2
+#define SOLENOID_FORCE_FLAG 0x80
 
 struct SolenoidDefinition
 {
@@ -22,7 +23,22 @@ public:
   void sleep();
   void solenoidOn(uint8_t index);
   void solenoidOff(uint8_t index);
+
+  /**
+   * Get the current state of all solenoids.
+   *
+   * Each bit represents the state of a solenoid, starting from solenoid 0 in the least significant bit.
+   * The most significant bit SOLENOID_FORCE_FLAG indicates that the actual state of solenoids is unknown.
+   */
   uint8_t getState() const;
+
+  /**
+   * Sets the current state of all solenoids.
+   *
+   * Normally solenoids are only activated when their state has changed.
+   * If the most significant bit SOLENOID_FORCE_FLAG is set in the current state or the requested state,
+   * then current state of solenoids is assumed to be unknown and all solenoids are activated.
+   */
   uint8_t setState(uint8_t state);
 };
 #endif
